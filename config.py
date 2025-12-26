d3de880e1d22713332153eb3cada1bd308bbfdfc
sreeharipradeep48-dot <sreeharipradeep48@gmail.com>
2025-12-26 14:55:51 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+API_ID = int(os.environ.get("API_ID", "123456"))
+API_HASH = os.environ.get("API_HASH", "YOUR_API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "YOUR_BOT_TOKEN")
+
+ADMINS = [123456789]
